7cccc283a965f04bc1c6cc2c5e5a633100e59714
Srujan <padakantisrujanchary@gmail.com>
2025-07-09 13:13:26 +0530

    changes done in notes routes

--- frontend/src/api/noteService.js
+++ frontend/src/api/noteService.js
@@ -1,6 +1,7 @@
 import api from "./api";
 
-export const createNote = (content) => api.post("/notes", { content });
-export const getNotes = () => api.get("/notes");
-export const updateNote = (id, content) => api.put(`/notes/${id}`, { content });
-export const deleteNote = (id) => api.delete(`/notes/${id}`);
+export const createNote = (content) => api.post("/api/notes", { content });
+export const getNotes = () => api.get("/api/notes");
+export const updateNote = (id, content) =>
+  api.put(`/api/notes/${id}`, { content });
+export const deleteNote = (id) => api.delete(`/api/notes/${id}`);
